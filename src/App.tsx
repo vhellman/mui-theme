247f87d57c8e74665c513a3ce212abe4f4b7de65
Viktor Hellman <hell_nam@hotmail.com>
2023-08-23 14:23:22 +0200

    Added some components

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Button from '@mui/material/Button';
 import { HeadlessButton } from "./components/HeadlessButton";
 import { ThemeProvider } from "@mui/material/styles";
 import { MaterialMigraTheme } from "./material-theme/MaterialMigraTheme";
-import { Checkbox, Select, TextField } from "@mui/material";
-
+import { Checkbox, InputLabel, Select, TextField, MenuItem, FormControl, Slider, CircularProgress, Chip, IconButton } from "@mui/material";
+import { Alarm } from "@mui/icons-material";
 function App() {
 
   return (
@@ -13,16 +13,32 @@ function App() {
     <main style={{display: "grid", gridTemplateColumns: "2fr 2fr", gap: "2rem", placeItems: "center", width: "100%", maxWidth: "1500px", minHeight: '100vh', padding: "3rem 4rem" }}>
       <div>
       <ThemeProvider theme={MaterialMigraTheme}>  
-        <DisplayWrapper developTime='' component={ <Button variant="contained">Material UI</Button> } /> 
-        <DisplayWrapper developTime='' component={ <Button variant="outlined">Material UI</Button> } /> 
-        <DisplayWrapper developTime='' component={ <Button variant="text">Material UI</Button> } /> 
-        <DisplayWrapper developTime='' component={ <TextField label="Outlined" /> } /> 
-        <DisplayWrapper developTime='' component={ <TextField variant="filled"/> } /> 
-        <DisplayWrapper developTime='' component={ <TextField variant="standard"/> } /> 
-        <DisplayWrapper developTime='' component={ <Checkbox  /> } /> 
-        <DisplayWrapper developTime='' component={ <Select  /> } /> 
+        <DisplayWrapper developTime='' component={ <Button variant="contained">Primär</Button> } /> 
+        <DisplayWrapper developTime='' component={ <Button variant="contained" color='secondary'>Sekundär</Button> } /> 
+        <DisplayWrapper developTime='' component={ <Button variant="outlined">Outlined</Button> } /> 
+        <DisplayWrapper developTime='' component={ <Button variant="mSecondary">MigraSek</Button> } /> 
+        <DisplayWrapper developTime='' component={ <Button variant="text">TextKnapp</Button> } /> 
+        <DisplayWrapper developTime='' component={ <Texter /> }/> 
+        <DisplayWrapper developTime='' component={ 
+          <FormControl fullWidth>
+            <InputLabel shrink htmlFor="bootstrap-input">
+          Select
+        </InputLabel>
+            <Select fullWidth label="Välj en sak">
+              <MenuItem value={"value1"} >Valett</MenuItem>
+              <MenuItem value={"value2"} >Valtvå</MenuItem>
+              <MenuItem value={"value3"} >Valtre</MenuItem>
+            </Select> 
+          </FormControl>
+          
+        } /> 
+        <DisplayWrapper developTime='' component={ <Checkbox /> } /> 
+        <DisplayWrapper developTime='' component={ <Slider color="primary" /> } /> 
+        <DisplayWrapper developTime='' component={ <Slider color="secondary" /> } /> 
+        <DisplayWrapper developTime='' component={ <CircularProgress /> } /> 
+        <DisplayWrapper developTime='' component={ <IconButton > <Alarm /></IconButton> } /> 
+        <DisplayWrapper developTime='' component={ <Chip color={"primary"} label={'Chips'} /> } /> 
       </ThemeProvider>
-77777bbbccccyyyy
       </div>
 
       <DisplayWrapper developTime='5min' component={ <HeadlessButton /> } /> 
@@ -30,6 +46,15 @@ function App() {
     </>
   )
 }
+
+const Texter = () => (
+    <FormControl >
+        <InputLabel shrink htmlFor="bootstrap-input">
+          Inputfält
+        </InputLabel>
+        <TextField InputLabelProps={{ shrink: true }} title="Outlined" /> 
+        </FormControl>
+)
 
 export default App
 
--- src/material-theme/MaterialMigraTheme.tsx
+++ src/material-theme/MaterialMigraTheme.tsx
@@ -6,6 +6,17 @@ declare module "@mui/material/styles/createPalette" {
             main: string,
             light: string
         }
+        mPrimary: {
+            main: string
+            hover: string
+            text: string
+        }
+        mSecondary: {
+            main: string
+            hover: string
+            text: string
+            dark: string
+        }
     }
 
     interface PaletteOptions {
@@ -13,7 +24,24 @@ declare module "@mui/material/styles/createPalette" {
             main: string,
             light: string
         }
+        mPrimary?: {
+            main: string
+            hover: string
+            text: string
+        }
+        mSecondary?: {
+            main: string
+            hover?: string
+            text?: string
+            dark: string
+        }
     }   
+}
+
+declare module '@mui/material/Button' {
+    interface ButtonPropsVariantOverrides {
+        mSecondary: true,
+    }
 }
 
 const MaterialMigraTheme = createTheme({
@@ -23,46 +51,90 @@ const MaterialMigraTheme = createTheme({
     },
     
     palette: {
-        primary: {
+        /* primary: {
             main: '#0d9488',
             dark: '#115e59',
             light: '#2dd4bf'
+        }, */
+        primary: {
+            main: '#25607f',
+            dark: '#143c50',
+            light: '#fff'
+            
+        },
+        secondary: {
+            main: '#b90835',
+            dark: '#8b1327'
+        }, 
+        mSecondary: {
+            main: '#f2f2f2',
+            dark: '#143c50',
         },
         migraWarning: {
             main: '#fb923c',
             light: '#fef3c7',
         }
     },
-    VafanHander?!16
     components: {
-        MuiButtonBase: {
+        MuiButton: {
             defaultProps: {
                 disableRipple: true
             },
             styleOverrides: {
                 root: {
+                    textTransform: 'initial',
                     '&.Mui-focusVisible': {
                         outline: '1px solid #707070',
-                        outlineOffset: '4px' 
+                        outlineOffset: '2px' 
+                    },
+                    '&.MuiButton-outlinedSecondary': {
+                        
                     }
                 },                
-            }
+            },
+            variants: [
+                {
+                    props: {variant: 'mSecondary'},
+                    style: {
+                        backgroundColor: '#f2f2f2',
+                        color: '#303030',
+                        border: '#707070 1px solid',
+                        '&:hover, &:focus':{
+                            borderColor: '#707070',
+                            backgroundColor: '#d9d9d9'
+                        }
+                    }
+                },
+                
+            ] 
+                    
         },
         MuiInputBase: {
-            
             styleOverrides: {
                 root: {
+                    color: '#303030',
+                    boxShadow: '3px 3px 10px rgba(0,0,0,0.16)',
                     '&.Mui-focused': {
                         outline: '1px solid #707070',
-                        outlineOffset: '4px' 
+                        outlineOffset: '2px' 
+                    },
+                    '& legend':{
+                        width: '0'
                     }
                 },                
             }
         },
-        
-        
+        MuiInputLabel: {
+            styleOverrides: {
+                root: {
+                    position: 'relative',
+                    transform: 'scale(0.85)',
+                }
+            }
+        }
 
     }
+    
 })
 
 export { MaterialMigraTheme }; 
